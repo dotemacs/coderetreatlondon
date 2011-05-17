3adf1eafe7960d942e11097d0926f546d4f1f971
Aleksandar Simic <asimic@gmail.com>
2011-05-17 20:11:58 +0000

    made the banner all caps

--- app.rb
+++ app.rb
@@ -14,18 +14,18 @@ __END__
 <html>
 <head>
 <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
-<title>CodeRetreat London</title> 
+<title>CodeRetreat London</title>
 </head>
 
 <body>
 <pre>
 <div class="header">
- ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ 
-||C |||o |||d |||e |||R |||e |||t |||r |||e |||a |||t ||
+ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____ ____
+||C |||O |||D |||E |||R |||E |||T |||R |||E |||A |||T ||
 ||__|||__|||__|||__|||__|||__|||__|||__|||__|||__|||__||
 |/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|/__\|
 
-                       London 
+                       London
 </div>
 <div class="content">
 
