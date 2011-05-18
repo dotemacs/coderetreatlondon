b8fccc5bc7a7180b76d5d0898cdfabf4bbadf050
Aleksandar Simic <asimic@gmail.com>
2011-05-18 15:08:46 +0000

    google analytics js added

--- app.rb
+++ app.rb
@@ -57,5 +57,18 @@ SPONSORS
   o <a href="http://peepcode.com/">Peepcode</a>
 </div>
 </pre>
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2248984-5']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
