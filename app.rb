303907044402bcd79eb4434001b9ca62d540c581
Aleksandar Simic <asimic@gmail.com>
2011-05-18 10:53:33 +0000

    changed the formatting

--- app.rb
+++ app.rb
@@ -31,7 +31,9 @@ __END__
 
 WHAT IS IT?
 
-  "Repeatable, day-long event that focuses on practicing the fundamentals of software development." - see <a href="http://coderetreat.ning.com/profiles/blogs/how-a-coderetreat-works">this</a> for more details.
+  "Repeatable, day-long event that focuses on practicing 
+  the fundamentals of software development." - see <a href="http://coderetreat.ning.com/profiles/blogs/how-a-coderetreat-works">this</a> 
+  for more details.
 
 WHEN AND WHERE
 
